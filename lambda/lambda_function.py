1a3a3a331e40a56b7559640cf91469db776ab344
Billy Moore <billy.moore98@hotmail.co.uk>
2025-05-06 19:04:52 +0100

    Add lambda function

---
+++ lambda/lambda_function.py
@@ -0,0 +1,42 @@
+import boto3
+import logging
+import os
+
+from src.api import PredictitAPI
+from typing import Optional
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+predictit = PredictitAPI()
+s3_client = boto3.client('s3')
+
+def lambda_handler(event, context) -> Optional[dict]:
+    """
+    Lambda function to poll the PredictIt API and store the data in S3
+
+    Params:
+        event (dict): The event data passed to the Lambda function
+        context (LambdaContext): The context object passed to the Lambda function
+    Returns:
+        Dict status message
+    """
+    try:
+        execution_timestamp = event.get('execution_timestamp')
+        # Poll the PredictIt API
+        logger.info('Polling PredictIt API market data now')
+        data = predictit.poll_market_data()
+        logger.info('Successfully polled API')
+        logger.info('Storing to S3 now')
+        bucket = os.getenv('S3_BUCKET')
+        if not bucket:
+            raise ValueError("S3_BUCKET environment variable is not set")
+        predictit.store_to_s3(data, bucket=bucket, filename=f'market_data_{execution_timestamp}.json')
+        logging.info('Successfully stored data to S3')
+        return {
+            'StatusCode': 200,
+            'message': 'PredictAPI data succcessfully polled and stored to S3'
+        }
+    except Exception as e:
+        logger.error(f'Error occurred: {e}')
+        raise
