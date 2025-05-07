c615b5baa40886db5058f8d2f79a05019f4e549a
billy-moore-98 <billy.moore98@hotmail.co.uk>
2025-05-07 10:22:11 +0100

    Add validate module

---
+++ src/validate.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class Contract(BaseModel):
+    id: int
+    dateEnd: str
+    image: str
+    name: str
+    shortName: str
+    status: str
+    lastTradePrice: Optional[float]
+    bestBuyYesCost: Optional[float]
+    bestBuyNoCost: Optional[float]
+    bestSellYesCost: Optional[float]
+    bestSellNoCost: Optional[float]
+    lastClosePrice: Optional[float]
+    displayOrder: Optional[float]
+
+class Market(BaseModel):
+    id: int
+    name: str
+    image: str
+    url: str
+    contracts: List[Contract]
+    timeStamp: str
+    status: str
+
+class PredictitResponse(BaseModel):
+    markets: List[Market]
