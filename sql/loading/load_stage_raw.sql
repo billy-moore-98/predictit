0e08a7a1b5562afaf78600c1e6f27f3e984d2cde
billy-moore-98 <billy.moore98@hotmail.co.uk>
2025-05-10 14:01:37 +0100

    Update params

--- sql/loading/load_stage_raw.sql
+++ sql/loading/load_stage_raw.sql
@@ -5,6 +5,6 @@ FROM (
     SELECT
         $1,
         METADATA$FILENAME
-    FROM @predictit_s3_stage/raw_data/market_data_{{ ts_nodash_with_t }}.json
+    FROM @predictit_s3_stage/raw_data/market_data_{{ params.execution_timestamp }}.json
     (FILE_FORMAT => predictit_json)
 );
