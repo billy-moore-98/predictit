cc60dbf83d9d30cf3304c2ac43b04316c7aa5d5c
billy-moore-98 <billy.moore98@hotmail.co.uk>
2025-05-30 14:39:51 +0100

    Add lambda_fetch tests

---
+++ tests/test_lambda_fetch.py
@@ -0,0 +1,39 @@
+import pytest
+
+from lambda_fetch.lambda_function import lambda_function, lambda_handler
+from src.api import PredictitAPI
+from unittest.mock import patch
+
+@patch('lambda_fetch.lambda_function.lambda_function')
+def test_lambda_handler_success(mock_lambda_func):
+    event = {'filename': 'test_data.json'}
+    response = lambda_handler(event, None)
+    mock_lambda_func.assert_called_once_with('test_data.json')
+    assert response == {
+        'StatusCode': 200,
+        'message': 'PredictAPI data succcessfully polled and stored to S3'
+    }
+
+@patch('lambda_fetch.lambda_function.lambda_function')
+def test_lambda_handler_no_filename(mock_lambda_func):
+    event = {}
+    with pytest.raises(ValueError, match="Filename must be provided in the event data"):
+        lambda_handler(event, None)
+
+@patch('lambda_fetch.lambda_function.os.getenv')
+@patch.object(PredictitAPI, 'store_to_s3')
+@patch.object(PredictitAPI, 'poll_market_data')
+def test_lambda_function_success(mock_poll, mock_store, mock_getenv):
+    mock_poll.return_value = {"markets": []}
+    mock_getenv.return_value = 'fake-bucket'
+
+    lambda_function("test.json")
+
+    mock_poll.assert_called_once()
+    mock_store.assert_called_once_with({"markets": []}, bucket='fake-bucket', filename='test.json')
+
+
+@patch('lambda_fetch.lambda_function.os.getenv', return_value=None)
+def test_lambda_function_no_s3_bucket(mock_getenv):
+    with pytest.raises(ValueError, match="S3_BUCKET environment variable is not set"):
+        lambda_function('test_data.json')
